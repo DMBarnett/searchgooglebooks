92f93c6e2e8a8b0228aff1f27c67af6f5b9a24d8
DMBarnett <davidmitchellbarnett@gmail.com>
2019-01-06 21:57:20 -0500

    not able to connect to mLab

--- server.js
+++ server.js
@@ -2,17 +2,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const path = require("path");
 
 const app = express();
 app.use(express.urlencoded({ extended:true }));
 app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 app.use(routes)
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 const PORT = process.env.port || 5000;
 
 const dbkey = require("./config/keys").mongoURI;
+console.log(dbkey);
 mongoose
-  .connect(dbkey)
+  .connect(dbkey, { useNewUrlParser: true})
   .then(()=>`Candygram for Mongo, database running`)
   .catch(err => console.log(err));
 
